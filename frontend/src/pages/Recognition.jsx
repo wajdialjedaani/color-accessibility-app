bc0f7d29d8ce8f7f8f1f6e8d03094d38cff312df
ntllong1997 <117335949+ntllong1997@users.noreply.github.com>
2023-10-12 20:13:53 -0500

    frame

---
+++ frontend/src/pages/Recognition.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Recognition() {
+  return (
+    <div>
+        <h1>Recognition</h1>
+    </div>
+  )
+}
+
+export default Recognition
---
+++ frontend/src/pages/Simulator.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Simulator() {
+  return (
+    <div>
+        <h1>Simulator</h1>
+    </div>
+  )
+}
+
+export default Simulator
