8fb6550e905fc2939eae4923ecb3b71c16e00b6d
nghiho112 <hieunghiho@gmail.com>
2023-10-12 20:19:55 -0500

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      asdasd
+      <Navbar />
+
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+// eslint-disable-next-line react/jsx-filename-extension
 root.render(<App tab="home" />);
