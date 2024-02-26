83b2de61a325d1ae119ec680f29fdc46b4a0f737
nghiho112 <hieunghiho@gmail.com>
2024-02-26 10:03:46 -0600

    sfad

--- frontend/src/app/api/chat/route.js
+++ frontend/src/app/api/chat/route.js
@@ -1,8 +1,10 @@
 import OpenAI from "openai";
 
+const key = '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';
+
 const openai = new OpenAI({
     organization: "org-ynEYrdDkdRGouaITKiJc2lcw",
-    apiKey: "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",
+    apiKey: key,
 });
 
 export async function POST(req) {
