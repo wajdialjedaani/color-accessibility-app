55b9373232b08f1944c827c5ead6de2a4042f3b0
ntllong1997 <117335949+ntllong1997@users.noreply.github.com>
2024-02-26 14:58:32 -0600

    chatbot styling

--- frontend/src/app/components/Chat.js
+++ frontend/src/app/components/Chat.js
@@ -1,31 +1,56 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import Toast from 'react-bootstrap/Toast';
 import ChatBox from './ChatBox';
 import logo from '../public/logo.png';
 import Image from 'next/image';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faComment} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComment } from '@fortawesome/free-solid-svg-icons';
 
 function Chat() {
     const [show, setShow] = useState(false);
-
+    const chatRef = useRef(null);
     const toggleShow = () => setShow(!show);
 
-  return (
-    <Container className="fixed-bottom d-flex flex-column align-items-end">
-        <Toast show={show} className="" style={{ backgroundColor: 'transparent' }}>
-          <ChatBox />
-        </Toast>
-        <FontAwesomeIcon 
-        onClick={toggleShow} 
-        src={logo} alt="Chat Icon"  
-        icon={faComment} size="2xl" 
-        style={{color: "#f4ac10", marginBottom: "1rem", cursor: "pointer"}} />
-    </Container>
-  );
+    const handleClickOutside = (e) => {
+        if (chatRef.current && !chatRef.current.contains(e.target)) {
+            setShow(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
+    return (
+        <Container className="fixed-bottom d-flex flex-column align-items-end">
+            <Toast
+                show={show}
+                className=""
+                style={{ backgroundColor: 'transparent' }}
+                ref={chatRef}
+            >
+                <ChatBox />
+            </Toast>
+            <FontAwesomeIcon
+                onClick={toggleShow}
+                src={logo}
+                alt="Chat Icon"
+                icon={faComment}
+                size="2xl"
+                style={{
+                    color: '#f4ac10',
+                    marginBottom: '1rem',
+                    cursor: 'pointer',
+                }}
+            />
+        </Container>
+    );
 }
 
 export default Chat;
