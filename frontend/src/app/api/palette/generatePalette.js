1b47beaa2015a5262a04290d8c37fcd2718f5312
anhthuntr <104386639+anhthuntr@users.noreply.github.com>
2024-03-04 00:17:04 -0600

    api fixed

--- frontend/src/app/api/palette/generatePalette.js
+++ frontend/src/app/api/palette/generatePalette.js
@@ -1,26 +1,57 @@
-export const generatePalette = (fn, colors) => {
-    const url = "http://colormind.io/api/";
-    const data = {
-      model: "default",
-      input: colors?.length === 0 ? undefined : colors.map(i => i.isLocked ? i.rgb : "N")
-    };
-    
-    const http = new XMLHttpRequest();
-  
-    http.onreadystatechange = function () {
-      if (http.readyState === 4 && http.status === 200) {
-        const result = JSON.parse(http.responseText).result;
+// export const generatePalette = (fn, colors) => {
+//     const url = "http://colormind.io/api/";
+//     const data = {
+//       model: "default",
+//       input: colors?.length === 0 ? undefined : colors.map(i => i.isLocked ? i.rgb : "N")
+//     };
 
-        const palette = result.map((i, idx) => {
-            return {
-                rgb: colors[idx]?.isLocked ? colors[idx]?.rgb : i, 
-                isLocked: colors[idx]?.isLocked ?? false
-            }
-        })
-        fn(palette);
-      }
-    };
+    
+//     const http = new XMLHttpRequest();
   
-    http.open("POST", url, true);
-    http.send(JSON.stringify(data));
-  };
+//     http.onreadystatechange = function () {
+//       if (http.readyState === 4 && http.status === 200) {
+//         const result = JSON.parse(http.responseText).result;
+//         console.log(result);
+//         const palette = result.map((i, idx) => {
+//             return {
+//                 rgb: colors[idx]?.isLocked ? colors[idx]?.rgb : i, 
+//                 isLocked: colors[idx]?.isLocked ?? false
+//             }
+//         })
+
+//         console.log(palette);
+//         fn(palette);
+//       }
+//     };
+  
+//     http.open("POST", url, true);
+//     http.send(JSON.stringify(data));
+//   };
+
+import axios from "axios";
+import { baseUrl } from "../../constants";
+
+export async function generatePalette(fn, colors) {
+    const apiUrl = `${baseUrl}generate-palette/`;
+    try {
+      const response = await axios.post(apiUrl, 
+        {
+          input: colors.map((color) => (color.isLocked ? color.rgb : "N")),
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    
+      if (response.status === 200) {
+        const data = response.data;
+        const palette = data.palette;
+        console.log(palette);
+        fn(palette);
+      } else {
+        console.error('Failed to fetch palette');
+      }
+    } catch (error) {
+      console.error('Error during fetch:', error);
+    }
+}
