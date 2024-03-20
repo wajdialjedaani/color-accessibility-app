f245e91591328bbc4752ebc328ce16b9d35a58b6
ntllong1997 <117335949+ntllong1997@users.noreply.github.com>
2024-03-20 18:40:22 -0500

    onLoad for integration and clear input in landing when reset

--- frontend/src/app/components/Integration.js
+++ frontend/src/app/components/Integration.js
@@ -3,11 +3,13 @@ import React, { useRef, useState, useEffect } from 'react';
 import ColorInfo from './ColorInfo';
 import { Image, Form, Button } from 'react-bootstrap';
 import { color_dict } from '../constants';
+import { LoadingState } from './LoadingState';
 
 const Integration = ({ sendPhase, file, labels }) => {
     const imageRef = useRef(null);
     const [isModified, setIsModified] = useState(false);
-    const [modifiedImage, setModifiedImage] = useState('');
+    const [imgSource, setImgSource] = useState(URL.createObjectURL(file));
+    const [imgData, setImgData] = useState();
     const [color, setColor] = useState({
         name: '',
         hex: '',
@@ -17,19 +19,73 @@ const Integration = ({ sendPhase, file, labels }) => {
     const [labelIndex, setLabelIndex] = useState(0);
 
     //const [colorGroups, setColorGroups] = useState({}); // State to store color groups
-    const [colorGroups, setColorGroups] = useState(Object.keys(color_dict).map((i) => ({ index: parseInt(i), pixels: [], url: '' }))); // { index: number; pixels: Array(number); url: string }
+    const [colorGroups, setColorGroups] = useState(
+        Object.keys(color_dict).map((i) => ({
+            index: parseInt(i),
+            pixels: [],
+            url: '',
+        }))
+    ); // { index: number; pixels: Array(number); url: string }
 
     useEffect(() => {
         const test = groupIndex(labels);
         setColorGroups(test);
     }, [labels]);
 
+    useEffect(() => {
+        if (
+            imageRef.current &&
+            imageRef.current.complete &&
+            imageRef.current.naturalWidth !== 0
+        ) {
+            const updatedColorGroups = colorGroups.map((group) => {
+                const canvas = document.createElement('canvas');
+                const ctx = canvas.getContext('2d');
+
+                canvas.width = imageRef.current.width;
+                canvas.height = imageRef.current.height;
+
+                ctx.drawImage(
+                    imageRef.current,
+                    0,
+                    0,
+                    canvas.width,
+                    canvas.height
+                );
+
+                const imgData = ctx.getImageData(
+                    0,
+                    0,
+                    canvas.width,
+                    canvas.height
+                );
+                const pixels = imgData.data;
+                setImgData(pixels);
+                const selectedGroup = group.pixels;
+
+                for (let i = 0; i < pixels.length; i += 4) {
+                    const dataIndex = i / 4;
+                    if (!selectedGroup.includes(dataIndex)) {
+                        pixels[i + 3] = pixels[i + 3] * 0.1;
+                    }
+                }
+
+                ctx.putImageData(imgData, 0, 0);
+                group.url = canvas.toDataURL();
+
+                return group;
+            });
+
+            setColorGroups(updatedColorGroups);
+        }
+    }, [file]);
+
     const groupIndex = (labels) => {
         const groups = [...colorGroups];
         Object.keys(labels).forEach((key) => {
             const label = labels[key];
-            const g = groups.find((i) => (i.index === parseInt(label)))
-            if (g){
+            const g = groups.find((i) => i.index === parseInt(label));
+            if (g) {
                 g.pixels.push(parseInt(key));
             }
         });
@@ -37,11 +93,10 @@ const Integration = ({ sendPhase, file, labels }) => {
         return groups;
     };
 
-
     const findColorName = async (input, label) => {
-        const red = input[0];
-        const green = input[1];
-        const blue = input[2];
+        const red = imgData[input * 4];
+        const green = imgData[input * 4 + 1];
+        const blue = imgData[input * 4 + 2];
         try {
             const response = await fetch(
                 `https://www.thecolorapi.com/id?rgb=${red},${green},${blue}&format=json`
@@ -57,7 +112,7 @@ const Integration = ({ sendPhase, file, labels }) => {
         }
     };
 
-    const getColorIndex = (e) => {
+    const handleImageClick = (e) => {
         const imageElement = imageRef.current;
         const rect = imageElement?.getBoundingClientRect();
 
@@ -68,66 +123,16 @@ const Integration = ({ sendPhase, file, labels }) => {
         const y = Math.floor(yRatio * imageElement.naturalHeight);
 
         const index = labels[y * imageElement.naturalWidth + x];
+        setLabelIndex(index);
+        findColorName(index, color_dict[index]);
 
         return index;
     };
 
-    const handleImageClick = (e) => {
-        const canvas = document.createElement('canvas');
-        const ctx = canvas.getContext('2d');
-
-        canvas.width = imageRef.current.width;
-        canvas.height = imageRef.current.height;
-
-        ctx.drawImage(imageRef.current, 0, 0, canvas.width, canvas.height);
-
-        const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        const pixels = imgData.data;
-        const colorIndex = getColorIndex(e);
-        setLabelIndex(colorIndex);
-        
-        // const selectedGroup = colorGroups[colorIndex];
-
-        // for (let i = 0; i < pixels.length; i += 4) {
-        //     const dataIndex = i / 4;
-        //     if (!selectedGroup.includes(dataIndex)) {
-        //         pixels[i + 3] = pixels[i + 3] * 0.1;
-        //     }
-        // }
-
-        ctx.putImageData(imgData, 0, 0);
-
-        findColorName(pixels, color_dict[colorIndex]);
-    };
-    
     const hightlightClicked = () => {
-        const canvas = document.createElement('canvas');
-        const ctx = canvas.getContext('2d');
-
-        canvas.width = imageRef.current.width;
-        canvas.height = imageRef.current.height;
-
-        ctx.drawImage(imageRef.current, 0, 0, canvas.width, canvas.height);
-
-        const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        const pixels = imgData.data;
-        const selectedGroup = colorGroups.find((i) => i.index === parseInt(labelIndex));
-        console.log(selectedGroup);
-
-        for (let i = 0; i < selectedGroup?.pixels.length; i += 4) {
-            const dataIndex = i / 4;
-            if (!selectedGroup.pixels.find((idx) => (idx === dataIndex))) {
-                pixels[i + 3] = pixels[i + 3] * 0.1;
-            }
-        }
-
-        const modifiedImageURL = canvas.toDataURL(); // Convert canvas to base64 URL
-
-
-        setModifiedImage(modifiedImageURL);
-        console.log(modifiedImageURL)
+        setImgSource(colorGroups[labelIndex].url);
         setIsModified(true);
-    }
+    };
 
     return (
         <div
@@ -140,69 +145,73 @@ const Integration = ({ sendPhase, file, labels }) => {
         >
             <div style={{ display: 'flex', justifyContent: 'center' }}>
                 <div style={{ maxHeight: '50rem' }}>
-                    {file && 
-                    <Image
-                        src={
-                            isModified
-                                ? modifiedImage
-                                : URL.createObjectURL(file)
-                        }
-                        ref={imageRef}
-                        alt="Image"
-                        fluid
-                        onClick={isModified ? null : handleImageClick}
-                        style={{ maxHeight: '50rem' }}
-                    />
-                }
+                    {file && (
+                        <Image
+                            src={imgSource}
+                            ref={imageRef}
+                            alt="Image"
+                            fluid
+                            onClick={isModified ? null : handleImageClick}
+                            style={{ maxHeight: '50rem' }}
+                        />
+                    )}
                 </div>
             </div>
             <div>
-                {labels?.length !== 0 &&
-                <div>
-                    <ColorInfo color={color} />
-                
+                {labels?.length !== 0 && (
+                    <div>
+                        <ColorInfo color={color} />
 
-                    <div style={{ marginTop: '2rem' }}>
-                        <h5>Color Highlight</h5>
-                        <div
-                            style={{
-                                height: '1px',
-                                width: '100%',
-                                backgroundColor: 'black',
-                            }}
-                        ></div>
-                        <div style={{ margin: '1rem auto 0.5rem auto' }}>
-                            Choose a Color
+                        <div style={{ marginTop: '2rem' }}>
+                            <h5>Color Highlight</h5>
+                            <div
+                                style={{
+                                    height: '1px',
+                                    width: '100%',
+                                    backgroundColor: 'black',
+                                }}
+                            ></div>
+                            <div style={{ margin: '1rem auto 0.5rem auto' }}>
+                                Choose a Color
+                            </div>
+                            <Form.Select
+                                aria-label="Default select example"
+                                style={{ marginTop: '1rem' }}
+                                value={labelIndex}
+                                onChange={(e) => {
+                                    setLabelIndex(e.target.value);
+                                }}
+                            >
+                                {colorGroups.map((i, idx) => (
+                                    <option value={i.index} key={idx}>
+                                        {color_dict[i.index]}
+                                    </option>
+                                ))}
+                            </Form.Select>
+                            <Button
+                                style={{
+                                    margin: '1rem',
+                                    border: 'none',
+                                }}
+                                onClick={hightlightClicked}
+                            >
+                                HIGHLIGHT
+                            </Button>
+                            <Button
+                                style={{
+                                    margin: '1rem auto',
+                                    border: 'none',
+                                }}
+                                onClick={() => {
+                                    setIsModified(false),
+                                        setImgSource(URL.createObjectURL(file));
+                                }}
+                            >
+                                RESET
+                            </Button>
                         </div>
-                        <Form.Select
-                            aria-label="Default select example"
-                            style={{ marginTop: '1rem' }}
-                            value={labelIndex}
-                            onChange={(e) => {setLabelIndex(e.target.value)}}
-                        >
-                            {colorGroups.map((i, idx) => <option value={i.index} key={idx} >{color_dict[i.index]}</option>)}
-                        </Form.Select>
-                        <Button
-                            style={{
-                                margin: '1rem',
-                                border: 'none'
-                            }}
-                            onClick={hightlightClicked}
-                        >
-                            HIGHLIGHT
-                        </Button>
-                        <Button
-                            style={{
-                                margin: '1rem auto',
-                                border: 'none'
-                            }}
-                            onClick={() => setIsModified(false)}
-                        >
-                            RESET
-                        </Button>
                     </div>
-                </div>
-                }
+                )}
             </div>
         </div>
     );
--- frontend/src/app/components/Landing.js
+++ frontend/src/app/components/Landing.js
@@ -1,22 +1,23 @@
 'use client';
 
 import { Button } from 'react-bootstrap';
-import React, { useState } from 'react';
-import { uploadImage } from '../api/image/route'
+import React, { useState, useRef } from 'react';
+import { uploadImage } from '../api/image/route';
 import { findSignificantColors } from '../api/palette/findSignificantColors';
-import Integration from '../components/Integration'
+import Integration from '../components/Integration';
 import { LoadingState } from './LoadingState';
 
 export default function Landing({ sendPhase }) {
     const [image, setImage] = useState(null);
     const [isLoad, setIsLoad] = useState(false);
-    const [labels, setLabels] = useState([])
+    const [labels, setLabels] = useState([]);
+    const fileInputRef = useRef(null); // Ref for the file input element
 
     const handleChange = (e) => {
         console.log(e.target.files);
         const file = e.target.files[0];
         setImage(file);
-        setLabels([])
+        setLabels([]);
     };
 
     const resizeImageByScale = (imageFile) => {
@@ -25,35 +26,37 @@ export default function Landing({ sendPhase }) {
             img.onload = () => {
                 const canvas = document.createElement('canvas');
                 const ctx = canvas.getContext('2d');
-                
+
                 const scale = chooseImageScale(img.width, img.height);
 
                 const newWidth = img.width * scale;
                 const newHeight = img.height * scale;
-    
+
                 canvas.width = newWidth;
                 canvas.height = newHeight;
-    
+
                 ctx.drawImage(img, 0, 0, newWidth, newHeight);
-    
+
                 canvas.toBlob((blob) => {
-                    resolve(new File([blob], imageFile.name, { type: 'image/jpeg' }));
+                    resolve(
+                        new File([blob], imageFile.name, { type: 'image/jpeg' })
+                    );
                 }, 'image/jpeg');
             };
-    
+
             img.src = URL.createObjectURL(imageFile);
         });
     };
 
     const chooseImageScale = (w, h) => {
-        const maxSize =  6 * (Math.pow(10, 5));
-        
-        if (w*h <= maxSize) return 1;
-        const scale = Math.sqrt(maxSize / ( w*h));
+        const maxSize = 6 * Math.pow(10, 5);
+
+        if (w * h <= maxSize) return 1;
+        const scale = Math.sqrt(maxSize / (w * h));
         console.log(scale);
         return scale;
         // return 1;
-    }
+    };
 
     const handleUpload = () => {
         if (image) {
@@ -65,55 +68,75 @@ export default function Landing({ sendPhase }) {
         setIsLoad(true);
 
         resizeImageByScale(image)
-            .then(resizedImage => {
-                uploadImage(resizedImage).then(res => {
-                    setImage(resizedImage)
-                    setLabels(res?.data?.label)
+            .then((resizedImage) => {
+                uploadImage(resizedImage).then((res) => {
+                    setImage(resizedImage);
+                    setLabels(res?.data?.label);
+                    setIsLoad(false);
+                });
+            })
+            .catch((error) => {
+                console.error('Error resizing image:', error);
                 setIsLoad(false);
             });
-        })
-        .catch(error => {
-            console.error('Error resizing image:', error);
-            setIsLoad(false); 
-        });
     };
 
     const handleClearImage = () => {
         setImage(null);
         setLabels([]);
-      };
+        if (fileInputRef.current) {
+            fileInputRef.current.value = '';
+        }
+    };
 
     return (
-        <div className="d-flex flex-column" style={{ margin: "2rem" }}>
+        <div className="d-flex flex-column" style={{ margin: '2rem' }}>
             <h3> Color Recognition</h3>
             <div style={{ marginTop: '1rem' }}>
-                <h5>Unlock the Potential of Every Hue in 3 Effortless Steps:</h5>
+                <h5>
+                    Unlock the Potential of Every Hue in 3 Effortless Steps:
+                </h5>
                 <ul>
                     <li>
-                    Upload Image: Choose any image, whether it's a photograph, artwork, or graphic, and effortlessly upload it with just a click.
+                        Upload Image: Choose any image, whether it's a
+                        photograph, artwork, or graphic, and effortlessly upload
+                        it with just a click.
                     </li>
 
                     <li>
-                    Identify Colors: With our intuitive system, explore your image and simply click on any part to reveal the color information instantly. Unveil the exact shades that catch your eye and delve into their RGB or HEX values with ease.
+                        Identify Colors: With our intuitive system, explore your
+                        image and simply click on any part to reveal the color
+                        information instantly. Unveil the exact shades that
+                        catch your eye and delve into their RGB or HEX values
+                        with ease.
                     </li>
 
                     <li>
-                    Discover Insights: Dive into the intricacies of your chosen colors, from vibrant primaries to subtle pastels. With each click, unlock a world of creative possibilities and gain valuable insights to elevate your projects.
+                        Discover Insights: Dive into the intricacies of your
+                        chosen colors, from vibrant primaries to subtle pastels.
+                        With each click, unlock a world of creative
+                        possibilities and gain valuable insights to elevate your
+                        projects.
                     </li>
                 </ul>
             </div>
-        <div style={{ marginTop: '1rem'}}>
-            <h5>Choose an Image</h5>
+            <div style={{ marginTop: '1rem' }}>
+                <h5>Choose an Image</h5>
                 <div>
                     <input
                         type="file"
                         accept="image/*"
-                        onChange={handleChange}  
+                        onChange={handleChange}
+                        ref={fileInputRef}
                     />
                 </div>
                 <Button
-                style={{ border: 'none', backgroundColor: '#39545B', margin: "1rem auto" }}
-                onClick={handleUpload}
+                    style={{
+                        border: 'none',
+                        backgroundColor: '#39545B',
+                        margin: '1rem auto',
+                    }}
+                    onClick={handleUpload}
                 >
                     GENERATE
                 </Button>
@@ -128,17 +151,15 @@ export default function Landing({ sendPhase }) {
                 >
                     RESET
                 </Button>
-        </div>
- 
-        {isLoad && (
-            <LoadingState />
-        )}
+            </div>
 
-        {image !== null &&
-        <div>
-        <Integration file={image} labels={labels}/>
-         </div>
-         }
-     </div>
+            {isLoad && <LoadingState />}
+
+            {image !== null && (
+                <div>
+                    <Integration file={image} labels={labels} />
+                </div>
+            )}
+        </div>
     );
 }
